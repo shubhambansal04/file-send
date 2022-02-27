77e18c70533e3eac137f0064ccddd8b6609fa4f5
shubhambansal04 <68951645+shubhambansal04@users.noreply.github.com>
2022-02-27 17:22:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 import base64
 import time
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(("192.168.1.100",2021))
+s.bind(("https://git.heroku.com/mysterious-plateau-17836.git",3000))
 s.listen(100000000)
 k=[]
 p=[]
